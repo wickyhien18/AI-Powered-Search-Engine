9d0c93cd4d9b468355ed6cb025e34fe658dbbaf8
wickyhien18 <giaphien1008@gmail.com>
2026-08-18 16:27:11 +0700

    build(deps): update setup.py

--- setup.py
+++ setup.py
@@ -8,3 +8,14 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 def download_bbc_dataset(output_dir: str = "./data"):
     api = KaggleApi()
     api.authenticate() 
+
+    print(f"Installing dataset into {output_dir} ...")
+    api.dataset_download_files(
+        "dimasmunoz/bbc-articles-cleaned",
+        path=output_dir,
+        unzip=True,
+    )
+    print("Done")
+
+if __name__ == "__main__":
+    download_bbc_dataset()
