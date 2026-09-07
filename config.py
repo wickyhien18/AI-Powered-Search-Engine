6fee65ea188ae4e12266565905ac07ac5bc40316
wickyhien18 <giaphien1008@gmail.com>
2026-09-07 18:11:19 +0700

    update config.py

--- config.py
+++ config.py
@@ -4,7 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 QDRANT_URL = os.getenv("QDRANT_URL", "http://localhost:6333")
-EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "nomic-embed-text")
-LLM_MODEL = os.getenv("LLM_MODEL", "llama3")
+QDRANT_API_KEY = os.getenv("QDRANT_API_KEY", None)
+
+EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "BAAI/bge-small-en-v1.5")
+EMBEDDING_DIM = 384
+
+LLM_MODEL = os.getenv("LLM_MODEL", "llama-3.1-8b-instant")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
 COLLECTION_NAME = os.getenv("COLLECTION_NAME", "bbc_news")
-EMBEDDING_DIM = 768
